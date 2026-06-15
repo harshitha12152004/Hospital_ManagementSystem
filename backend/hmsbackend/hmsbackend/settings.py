f43333b416347163a350192c715abfd6d4411fcc
Gangishetty Harshitha <harshithagangishettyharshitha@gmail.com>
2026-06-15 16:03:04 +0530

    Update settings.py

--- backend/hmsbackend/hmsbackend/settings.py
+++ backend/hmsbackend/hmsbackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ojg$k^8()$aa54%y*_+)vtl0po#0wln8r78jye0xt@dib0)(=r'
+# SECURITY WARNING: keep the secret key used in production 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-key-for-local-only')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
