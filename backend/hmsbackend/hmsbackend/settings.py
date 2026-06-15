cd8239215a39ee7381a6024d75dae4f0051bea58
Gangishetty Harshitha <harshithagangishettyharshitha@gmail.com>
2026-06-15 19:56:30 +0530

    Update settings.py

--- backend/hmsbackend/hmsbackend/settings.py
+++ backend/hmsbackend/hmsbackend/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'accounts',
-    'bookings',
+    'hmsbackend.accounts.apps.AccountsConfig',
+    'hmsbackend.bookings.apps.BookingsConfig',
     'hmsbackend'
 ]
 
