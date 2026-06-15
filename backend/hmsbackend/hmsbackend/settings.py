9245ac22607f6b96a855eb9ad5705b8e3ee6fb48
Gangishetty Harshitha <harshithagangishettyharshitha@gmail.com>
2026-06-15 19:06:51 +0530

    Update app references in Django settings

--- backend/hmsbackend/hmsbackend/settings.py
+++ backend/hmsbackend/hmsbackend/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'hmsbackend.accounts',
-    'hmsbackend.bookings',
+    'hmsbackend.accounts.apps.AccountsConfig',
+    'hmsbackend.bookings.apps.BookingsConfig',
     'hmsbackend'
 ]
 
