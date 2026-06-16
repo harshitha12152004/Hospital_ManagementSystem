9cfb895acec21e8ed86d0de1085e16a4c5b3d1c1
Gangishetty Harshitha <harshithagangishettyharshitha@gmail.com>
2026-06-16 15:40:45 +0530

    Update import path for User model in models.py

--- backend/hmsbackend/bookings/models.py
+++ backend/hmsbackend/bookings/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import User
+from hmsbackend.accounts.models import User
 
 class AvailabilitySlot(models.Model):
     doctor = models.ForeignKey(User, on_delete=models.CASCADE)
